cd2e39e94a68d013519165c589181d367eff0a3c
ramis <ramis94@yandex.ru>
2022-03-18 00:07:02 +0300

    removed SafeMath

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -2,11 +2,9 @@ pragma solidity ^0.8.0;
 
 import "./IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract SimpleToken is IERC20, Ownable {
 
-    using SafeMath for uint;
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
@@ -21,8 +19,8 @@ contract SimpleToken is IERC20, Ownable {
     }
 
     function transfer(address recipient, uint amount) override external returns (bool) {
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
-        _balances[recipient] = _balances[recipient].add(amount);
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
@@ -42,22 +40,22 @@ contract SimpleToken is IERC20, Ownable {
         address recipient,
         uint amount
     ) override external returns (bool) {
-        _allowances[sender][recipient] = _allowances[sender][recipient].sub(amount);
-        _balances[sender] = _balances[sender].sub(amount);
-        _balances[recipient] = _balances[recipient].add(amount);
+        _allowances[sender][recipient] = _allowances[sender][recipient] - amount;
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
         emit Transfer(sender, recipient, amount);
         return true;
     }
 
     function mint(uint amount) external onlyOwner {
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
-        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] += amount;
+        _totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
 
     function burn(uint amount) external onlyOwner {
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
-        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] -= amount;
+        _totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
     }
 }
